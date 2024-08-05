74c1c0928dd8f262d032e1796b9e01e996067565
jesukka123 <165469028+jesukka123@users.noreply.github.com>
2024-08-05 22:56:32 +0300

    Add files via upload

---
+++ kamux/fxmanifest.lua
@@ -0,0 +1,8 @@
+shared_script '@WaveShield/waveshield.lua' --this line was automatically written by WaveShield
+
+game 'gta5'
+fx_version 'cerulean'
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+this_is_a_map 'yes'
+lua54 'yes'
